c810c07e45f65d9587799273da426c6d277d230c
aidoomu <67350960+aidoomusahrazal@users.noreply.github.com>
2023-02-15 10:00:41 +0000

    Hello this is just the beginning

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styles.css">
+    <title>Try out</title>
+</head>
+<body>
+    <nav>
+        <h2>Login</h2>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Login</a></li>
+            <li><a href="">Sign up</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
